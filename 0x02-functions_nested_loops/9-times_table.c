84ddfd958345608d3a4c6688e3e1d98b486f4a83
kay-mastermind <hermandre32@gmail.com>
2023-07-18 15:27:14 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,22 +4,22 @@
 
 void times_table(void)
 {
-	int i, j, result;
+int i, j, result;
 
-	for (i = 0; i < 10 ; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			result = i * j;
-			if (j == 0)
-				printf("%d, ", result);
-			else
-			{
-				printf("%2d", result);
-				if (j != 9)
-					printf(", ");
-			}
-		}
-		printf("/n");
-	}
+for (i = 0; i < 10 ; i++)
+{
+for (j = 0; j < 10; j++)
+{
+result = i * j;
+if (j == 0)
+printf("%d, ", result);
+else
+{
+printf("%2d", result);
+if (j != 9)
+printf(", ");
+}
+}
+printf("/n");
+}
 }
